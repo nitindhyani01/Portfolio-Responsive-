72bab1d886d16a81592327854487f8d558f15f13
Nitin Dhyani <nitindhyani1234@gmail.com>
2024-08-01 09:24:56 +0530

    javascript added

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-var togglebtn=document.querySelector(".togglebtn");
-var nav=document.querySelector(".navlinks");
-var links=document.querySelector(".navlinks li");
+var togglebtn = document.querySelector(".togglebtn");
+var nav = document.querySelector(".navlinks");
+var links = document.querySelector(".navlinks li");
 
-togglebtn.addEventListener("click",function(){
-    this.classList.toggle("click");
-    nav.classList.toggle("open");
+togglebtn.addEventListener("click", function () {
+  this.classList.toggle("click");
+  nav.classList.toggle("open");
 });
 
-var typed = new typed(".input",{
-    strings:["Lerner,Developer,curious"],
-    typedSpeed:70,
-    backSpeed:55,
-    loop:true,
-});
+var typed = new Typed(".input", {
+  strings: ["Lerner","Developer","Curious"],
+  typedSpeed: 70,
+  backSpeed: 55,
+  loop: true
+})
